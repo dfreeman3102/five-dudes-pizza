21d90dff4e170f222a999b05c1f15e1545f1fbf1
Jinkc21 <jin.k.choe@gmail.com>
2024-03-19 18:54:35 -0400

    styled hawaiian pizza and modified code for api calls

--- assets/js/menu2.js
+++ assets/js/menu2.js
@@ -33,9 +33,12 @@
 //   }
 // };
 // api.analyzeRecipe(analyzeRecipeRequest, opts, callback);
-const apiKey =
-  "0bd84def3a95469c8cc5ddc3c2736a4e"
-"41342be6e3224906b0a5512ceb92ef9e"
+// const apiKey =
+
+// "aa6cfeb66b1c442aa09657728a431c94"
+
+  // "0bd84def3a95469c8cc5ddc3c2736a4e"
+// "41342be6e3224906b0a5512ceb92ef9e"
 
 const marinaraUrl = `https://api.spoonacular.com/food/ingredients/search?query=marinara&sort=calories&sortDirection=desc&apiKey=${apiKey}&includeNutrition=true`
 const mozzarellaUrl = `https://api.spoonacular.com/food/ingredients/search?query=mozzarella&sort=calories&sortDirection=desc&apiKey=${apiKey}&includeNutrition=true`
@@ -52,45 +55,55 @@ let ingredient = []
 // const ingredientId = data.results[0].id
 
 
-const ingredientUrl = `https://api.spoonacular.com/food/ingredients/search?query=${ingredient}&sort=calories&sortDirection=desc&apiKey=${apiKey}&includeNutrition=true`
-function getIngredient() {
-  var ingredient = "bell pepper"
-  fetch(ingredientUrl)
-    .then(function (results) {
-      return results.json();
+// const ingredientUrl = `https://api.spoonacular.com/food/ingredients/search?query=${ingredient}&sort=calories&sortDirection=desc&apiKey=${apiKey}&includeNutrition=true`
+// function getIngredient() {
+//   var ingredient = "bell pepper"
+//   fetch(ingredientUrl)
+//     .then(function (results) {
+//       return results.json();
 
-    })
-    .then(function (data) {
-      const ingredientId = data.results[0].id
-      // console.log(ingredientId);
-      // getNutrition()
+//     })
+//     .then(function (data) {
+//       const ingredientId = data.results[0].id
+//       // console.log(ingredientId);
+//       // getNutrition()
 
-      function getNutrition() {
-        const nutritionUrl = `https://api.spoonacular.com/food/ingredients/${ingredientId}/information?amount=1&apiKey=${apiKey}`
-        fetch(nutritionUrl)
+//       function getNutrition() {
+//         const nutritionUrl = `https://api.spoonacular.com/food/ingredients/${ingredientId}/information?amount=1&apiKey=${apiKey}`
+//         fetch(nutritionUrl)
 
-          .then(function (results) {
-            return results.json();
-          })
-          .then(function (data) {
-            // console.log(data);
-            // const nutritionInfo = 
+//           .then(function (results) {
+//             return results.json();
+//           })
+//           .then(function (data) {
+//             // console.log(data);
+//             // const nutritionInfo = 
 
-          })
-      }
-      getNutrition()
-    })
-}
+//           })
+//       }
+//       getNutrition()
+//     })
+// }
 
 // getIngredient()
 
 const vegNutritionEl = document.getElementById("veggie-nutrition")
 const caloriesArr = []
+var ingredientId = ""
 const nutritionUrl = `https://api.spoonacular.com/food/ingredients/${ingredientId}/information?amount=1&apiKey=${apiKey}`
 
 
-function veggieNutrition() {
-  const marinaraNutritionUrl = ``
+function getNutrition() {
+  const crustNutritionUrl = `https://api.spoonacular.com/food/ingredients/93770/information?amount=1&apiKey=${apiKey}`
+  fetch(crustNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const crustCalories = data.nutrition.nutrients[10].amount
+      console.log(crustCalories)
+    })
+  const marinaraNutritionUrl = `https://api.spoonacular.com/food/ingredients/10111549/information?amount=1&apiKey=${apiKey}`
   fetch(marinaraNutritionUrl)
     .then(function (results) {
       return results.json();
@@ -99,8 +112,6 @@ function veggieNutrition() {
       const marinaraCalories = data.nutrition.nutrients[10].amount
       console.log(marinaraCalories)
     })
-
-
 
   const mozzNutritionUrl = `https://api.spoonacular.com/food/ingredients/1001026/information?amount=1&apiKey=${apiKey}`
   fetch(mozzNutritionUrl)
@@ -112,7 +123,7 @@ function veggieNutrition() {
       console.log(mozzCalories)
     })
 
-  const mushroomNutritionUrl = ``
+  const mushroomNutritionUrl = `https://api.spoonacular.com/food/ingredients/11260/information?amount=1&apiKey=${apiKey}`
   fetch(mushroomNutritionUrl)
     .then(function (results) {
       return results.json();
@@ -122,7 +133,7 @@ function veggieNutrition() {
       console.log(mushroomCalories)
     })
 
-  const bellNutritionUrl = ``
+  const bellNutritionUrl = `https://api.spoonacular.com/food/ingredients/10211821/information?amount=1&apiKey=${apiKey}`
   fetch(bellNutritionUrl)
     .then(function (results) {
       return results.json();
@@ -131,7 +142,7 @@ function veggieNutrition() {
       const bellPepperCalories = data.nutrition.nutrients[10].amount
       console.log(bellPepperCalories)
     })
-  const onionNutritionUrl = ``
+  const onionNutritionUrl = `https://api.spoonacular.com/food/ingredients/11282/information?amount=1&apiKey=${apiKey}`
   fetch(onionNutritionUrl)
     .then(function (results) {
       return results.json();
@@ -140,16 +151,72 @@ function veggieNutrition() {
       const onionCalories = data.nutrition.nutrients[10].amount
       console.log(onionCalories)
     })
-    const olivesNutritionUrl = ``
-    fetch(olivesNutritionUrl)
-      .then(function (results) {
-        return results.json();
-      })
-      .then(function (data) {
-        const olivesCalories = data.nutrition.nutrients[10].amount
-        console.log(olivesCalories)
-      })
-  }
-  
+  const olivesNutritionUrl = `https://api.spoonacular.com/food/ingredients/1059195/information?amount=1&apiKey=${apiKey}`
+  fetch(olivesNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const olivesCalories = data.nutrition.nutrients[10].amount
+      console.log(olivesCalories)
+    })
+  const pepperoniNutritionUrl = `https://api.spoonacular.com/food/ingredients/7057/information?amount=1&apiKey=${apiKey}`
+  fetch(pepperoniNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const pepperoniCalories = data.nutrition.nutrients[10].amount
+      console.log(pepperoniCalories)
+    })
+   const sausageNutritionUrl = `https://api.spoonacular.com/food/ingredients/7036/information?amount=1&apiKey=${apiKey}`
+  fetch(sausageNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const sausageCalories = data.nutrition.nutrients[10].amount
+      console.log(sausageCalories)
+    })
+  const baconNutritionUrl = `https://api.spoonacular.com/food/ingredients/10123/information?amount=1&apiKey=${apiKey}`
+  fetch(baconNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const baconCalories = data.nutrition.nutrients[10].amount
+      console.log(baconCalories)
+    })
+  const hamNutritionUrl = `https://api.spoonacular.com/food/ingredients/10151/information?amount=1&apiKey=${apiKey}`
+  fetch(hamNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const hamCalories = data.nutrition.nutrients[10].amount
+      console.log(hamCalories)
+    })
+  const pineappleNutritionUrl = `https://api.spoonacular.com/food/ingredients/10151/information?amount=1&apiKey=${apiKey}`
+  fetch(pineappleNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const pineappleCalories = data.nutrition.nutrients[10].amount
+      console.log(pineappleCalories)
+    })
+  const oliveOilNutritionUrl = `https://api.spoonacular.com/food/ingredients/10151/information?amount=1&apiKey=${apiKey}`
+  fetch(oliveOilNutritionUrl)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (data) {
+      const oliveOilCalories = data.nutrition.nutrients[10].amount
+      console.log(oliveOilCalories)
+    })
 
-veggieNutrition()
+}
+
+
+
+getNutrition()
