a4ff6cb351bca333e5a54ffe359886d1e66c34af
ijmckoy <Ikeman200@gmail.com>
2024-03-19 21:13:50 -0400

    add java to menu script

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,13 @@
 var addToOrder = document.querySelector(".addOrder");
- var containerAPI = document.querySelector("#pepperoni-desc");
+ var containerAPI = document.querySelector("#pepperoni-nutrition");
+ var pep = document.querySelector("#pepperoni1");
 var totalcal = 0;
-var totalCalcium = 0;
- var ingred = ["Pepperoni","mozzarella" ," marinara sauce"];
+var totalFat = 0;
+var totalProtein = 0;
+var ingred =  ["Pepperoni","mozzarella" ," marinara sauce"];
+var ingred2 =  ["basil","mozzarella" ," marinara sauce", "olive oil"];
+var ingred3 =  ["Marinara sauce", "sliced garlic", "olive oil"];
+
 
 function getPizza() { 
 
@@ -16,23 +21,55 @@ return response.json();
 })
 .then(function(data){
     console.log( data);
-   var calories = data.calories;
-  var  calcium = data.totalNutrients.CA.quantity;
+     var calories = data.calories;
+    var  Fat = data.totalNutrients.FAT.quantity;
+    var Protein = data.totalNutrients.PROCNT.quantity;
 
-     totalcal = totalcal + calories
-     totalCalcium = totalCalcium + calcium;
-
+     totalcal = totalcal + calories;
+     totalFat = totalFat + Fat;
+    totalProtein = totalProtein + Protein
 
      console.log("calories " + calories);
      console.log("total cal: "  + totalcal);
-     containerAPI.textContent = " Total cal: " + totalcal + " Total calcium: " + totalCalcium +"mg";
+     containerAPI.textContent = " Total cal: " + totalcal + " Total Fat: " + totalFat +"g " +" Total Protien: " + totalProtein;
     
 })
 }
 
+for ( var i = 0; i < ingred2.length; i++){
+
+    var requestUrl = 'https://api.edamam.com/api/nutrition-data?app_id=d0c44e23&app_key=c02589e218b92f37f70c91171fde6952&nutrition-type=logging&ingr='+ ingred[i]
+    fetch(requestUrl)
+    .then(function(response){
+       
+    return response.json();
+    })
+    .then(function(data){
+        console.log( data);
+         var calories = data.calories;
+        var  Fat = data.totalNutrients.FAT.quantity;
+        var Protein = data.totalNutrients.PROCNT.quantity;
+    
+         totalcal = totalcal + calories;
+         totalFat = totalFat + Fat;
+        totalProtein = totalProtein + Protein
+    
+         console.log("calories " + calories);
+         console.log("total cal: "  + totalcal);
+         containerAPI.textContent = " Total cal: " + totalcal + " Total Fat: " + totalFat +"g " +" Total Protien: " + totalProtein;
+        
+    })
+    }
+
+
+
+
+
+
+
 }
 
-
+getPizza();
 
 
 
@@ -53,5 +90,5 @@ console.log( data2);
 
 
 
-getPizza();
+
 
